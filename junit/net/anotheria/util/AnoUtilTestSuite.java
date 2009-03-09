99e0f5b3354255497093b807d63ef8c33306bf95
Leon Rosenberg <leon.rosenberg@anotheria.net>
2009-03-09 08:36:09 +0000

    added TestSuite

---
+++ junit/net/anotheria/util/AnoUtilTestSuite.java
@@ -0,0 +1,11 @@
+package net.anotheria.util;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(value=Suite.class)
+@SuiteClasses(value=IdCodeGeneratorTest.class )
+public class AnoUtilTestSuite {
+
+}
