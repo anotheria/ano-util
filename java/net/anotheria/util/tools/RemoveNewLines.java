7019975da21dc9409cf148eac91b48d1c2fafd05
Leon Rosenberg <leon.rosenberg@anotheria.net>
2009-05-23 19:23:34 +0000

    checkstyle

--- java/net/anotheria/util/tools/RemoveNewLines.java
+++ java/net/anotheria/util/tools/RemoveNewLines.java
@@ -5,7 +5,17 @@ import java.io.FileOutputStream;
 import net.anotheria.util.IOUtils;
 import net.anotheria.util.StringUtils;
 
-public class RemoveNewLines {
+/**
+ * A simple command line programm that removes all \r from the file submitted as argument.
+ * @author lrosenberg
+ *
+ */
+public final class RemoveNewLines {
+	/**
+	 * Removes empty lines from the file at a[0]
+	 * @param a do you need a comment on that?
+	 * @throws Exception if something happens.
+	 */
 	public static void main(String a[]) throws Exception{
 		String s = IOUtils.readFileAtOnceAsString(a[0]);
 		s = StringUtils.removeChar(s, '\r');
@@ -13,4 +23,6 @@ public class RemoveNewLines {
 		fOut.write(s.getBytes());
 		fOut.close();
 	}
+	
+	private RemoveNewLines(){}
 }
