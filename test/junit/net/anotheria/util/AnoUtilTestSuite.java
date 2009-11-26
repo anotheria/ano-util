4292ac18e4c7dfc43fdd261eeea8f5029227bd6d
Denis Metelin <denis.metelin@nazax.com>
2009-11-26 15:48:58 +0000

    IPFIltering: java-docs+unit tests

--- test/junit/net/anotheria/util/AnoUtilTestSuite.java
+++ test/junit/net/anotheria/util/AnoUtilTestSuite.java
@@ -1,6 +1,8 @@
 package net.anotheria.util;
 
 import net.anotheria.util.crypt.CryptToolTest;
+import net.anotheria.util.network.IPRangeTest;
+import net.anotheria.util.network.PlainIPFilterTest;
 import net.anotheria.util.slicer.SlicerTest;
 import net.anotheria.util.sorter.SorterTest;
 
@@ -10,7 +12,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(value=Suite.class)
 @SuiteClasses(value={IdCodeGeneratorTest.class, StringUtilsTest.class, BasicComparableTest.class, CharacterEntityCoderTest.class, DateTest.class, DateUtilityTest.class, CryptToolTest.class, 
-		MD5UtilTest.class, ExecutionTimerTest.class, NumberUtilsTest.class, SlicerTest.class, SorterTest.class, ArrayUtilsTest.class} )
+		MD5UtilTest.class, ExecutionTimerTest.class, NumberUtilsTest.class, SlicerTest.class, SorterTest.class, ArrayUtilsTest.class, IPRangeTest.class, PlainIPFilterTest.class} )
 public class AnoUtilTestSuite {
 	
 }
