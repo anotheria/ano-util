2f03942d2336d9b428c35584ac31ffd4bdf0ea20
Leon Rosenberg <leon.rosenberg@anotheria.net>
2014-08-15 15:25:48 +0200

    moved java/net to src/main/java

--- java/net/anotheria/util/ISubjectProxy.java
+++
@@ -1,9 +0,0 @@
-package net.anotheria.util;
-
-/**
- * @author another
- *
- */
-public interface ISubjectProxy extends IObserver, ISubject{
-
-}
--- java/net/anotheria/util/tools/Worker.java
+++
@@ -1,7 +0,0 @@
-package net.anotheria.util.tools;
-
-import java.io.File;
-
-public interface Worker {
-	void processFile(File file);
-}	
