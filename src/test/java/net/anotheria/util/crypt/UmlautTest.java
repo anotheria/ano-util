2a6446ab4a8cf3fcc0d8aca6ca261666b2155caa
Leon Rosenberg <leon.rosenberg@anotheria.net>
2020-08-21 00:37:03 +0200

    attempt to fix test for infra

--- src/test/java/net/anotheria/util/crypt/UmlautTest.java
+++ src/test/java/net/anotheria/util/crypt/UmlautTest.java
@@ -15,10 +15,10 @@ public class UmlautTest {
 		CryptTool tool = new CryptTool("3CBAAC08769023063897E8E4B9CAEC61");
 		String message = "ÄÖÜÖÄÜüöäöüä";
 		byte[] encrypted = tool.encrypt(message);
-		System.out.println(new String(encrypted));
+		System.out.println(new String(encrypted, "UTF-8"));
 		System.out.println(tool.encryptToHex(message));
 		byte[] decrypted = tool.decrypt(encrypted);
-		String restoredMessage = new String(decrypted).trim();
+		String restoredMessage = new String(decrypted, "UTF-8").trim();
 		System.out.println("DEC."+restoredMessage+".");
 		assertEquals(message, restoredMessage);
 	}
@@ -32,14 +32,14 @@ public class UmlautTest {
 		//System.out.println("\u00FC");
 		//System.out.println(Arrays.toString("\u00D6".getBytes("UTF-8")));
 		byte[] m = {-61, -92, -61, -68, -61, -106,25, 66, -61, -92, -61, -68, -61, -106};
-		String message = new String(m);
+		String message = new String(m, "UTF-8");
 		System.out.println(message);
 
 
 		CryptTool tool = new CryptTool("3CBAAC08769023063897E8E4B9CAEC61");
 		byte[] encrypted = tool.encrypt(message);
 		byte[] decrypted = tool.decrypt(encrypted);
-		String decryptedMessage  = new String(decrypted).trim();
+		String decryptedMessage  = new String(decrypted, "UTF-8").trim();
 
 		assertEquals(decryptedMessage, message);
 
