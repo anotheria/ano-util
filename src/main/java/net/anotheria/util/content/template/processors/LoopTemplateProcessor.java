7eb121d25df49990f11b65eb93a240f4cba5e030
HELLnyk <yurock.ua@gmail.com>
2025-10-01 12:56:08 +0300

    adjustments for loop template processor

--- src/main/java/net/anotheria/util/content/template/processors/LoopTemplateProcessor.java
+++ src/main/java/net/anotheria/util/content/template/processors/LoopTemplateProcessor.java
@@ -1,15 +1,13 @@
 package net.anotheria.util.content.template.processors;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import net.anotheria.util.content.template.TemplateProcessor;
 import net.anotheria.util.content.template.TemplateReplacementContext;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * LoopProcessor — processing list parameter.
@@ -18,6 +16,11 @@ import java.util.Map;
  * @since 24.09.2025
  */
 public class LoopTemplateProcessor implements TemplateProcessor {
+    /**
+     * Utility instance for mapping different collection instances to map.
+     */
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
     @Override
     public String replace(String aPrefix, String aVariable, String aDefValue, TemplateReplacementContext aContext) {
         Object data = aContext.getAttribute(aVariable);
@@ -28,37 +31,28 @@ public class LoopTemplateProcessor implements TemplateProcessor {
         if (collection.isEmpty())
             return "";
 
+        List<Map<String, String>> listData = collection.stream()
+                .map(this::mapElement)
+                .collect(Collectors.toList());
+
         StringBuilder result = new StringBuilder();
-        List<Map<String, String>> listData = buildData(collection);
         for (Map<String, String> element: listData){
             String processed = aDefValue;
-            for (String key: element.keySet()) {
-                processed = processed.replaceAll(aVariable + "." + key, element.get(key));
-            }
+            for (String key: element.keySet())
+                processed = processed.replace(aVariable + "." + key, element.get(key));
+
             result.append(processed).append("\n");
         }
         return result.toString().trim();
     }
 
-    private List<Map<String, String>> buildData(Collection<?> collection) {
-        List<Map<String, String>> ret = new ArrayList<>();
-        for (Object item : collection) {
-            Map<String, String> map = new HashMap<>();
-            for (Field field : item.getClass().getDeclaredFields()) {
-                try {
-                    String getterName = "get" + Character.toUpperCase(field.getName().charAt(0)) + field.getName().substring(1);
-                    Method method = item.getClass().getMethod(getterName);
-                    Object value = method.invoke(item);
-                    map.put(field.getName(), value != null ? value.toString() : null);
-                } catch (Exception e) {
-                    try {
-                        Object value = field.get(item);
-                        map.put(field.getName(), value != null ? value.toString() : null);
-                    } catch (Exception ignore) {}
-                }
-            }
-            ret.add(map);
-        }
-        return ret;
+    private Map<String, String> mapElement(Object obj) {
+        Map<String, Object> rawMap = MAPPER.convertValue(obj, Map.class);
+        return rawMap.entrySet().stream()
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        e -> e.getValue() == null ? "" : e.getValue().toString(),
+                        (value1, value2) -> value1
+                ));
     }
 }
